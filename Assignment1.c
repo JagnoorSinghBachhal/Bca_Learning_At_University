86bdc35c4afaae6d78e9e9246231aeaa7b2f9c1d
JagnoorSinghBachhal <127727334+JagnoorSinghBachhal@users.noreply.github.com>
2023-03-18 19:38:46 +0530

    Update Assignment1.c

--- Assignment1.c
+++ Assignment1.c
@@ -1,9 +1,5 @@
 //Write a C program that allows the user to enter 'N' numbers and find the total positive numbers and total negative numbers using a for loop.
-/*
-Made by:-
-Jagnoor Singh Bachhal-2210997105
 
-*/
 #include<stdio.h>
 int main()
 {
